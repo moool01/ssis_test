e4f4d3b144e97cccdb0069e467f930e9c94d30aa
PangPangGod <rhcp1134@gmail.com>
2023-10-26 09:44:34 +0900

    231026 PDF BaseLine

--- baseline_for_pdf.py
+++ baseline_for_pdf.py
@@ -1,31 +1,31 @@
 """ dependency : pypdf. """
 
-class Extract_Text_From_PDF() :
-    def __init__(self, file) :
+class Extract_Text_From_PDF():
+    def __init__(self, file_path):
         """ Get File From Local and Wrap with pypdf.PdfReader. """
 
-        try : import pypdf
-        except ImportError :      
+        try: import pypdf
+        except ImportError:      
             print(""" Error Occured. Try pip install pypdf. """)
             exit(0)
 
-        self.file = pypdf.PdfReader(file)
+        self.file = pypdf.PdfReader(file_path)
 
-    def extract_text(self, start_page=1, end_page=None, path="./result.txt") :
+    def extract_text(self, start_page=1, end_page=None, path="./result.txt"):
         """ 객체에 추출한 텍스트 담아서 path에 지정된 파일에 저장. start page부터 end page까지. """
 
         text = ""
 
-        for i in range(start_page-1, end_page+1) :
+        for i in range(start_page-1, end_page):
             text += f'\n============================================================\nStart of Page {i+1}.\n============================================================\n\n'
             text += self.file.pages[i].extract_text()
             text += f'\n\n============================================================\nEnd of Page {i+1}.\n============================================================\n'
 
-        with open(path, "w", encoding='utf-8') as file :
+        with open(path, "w", encoding='utf-8') as file:
             file.write(text)
         
-if __name__ == "__main__" :
-    extract = Extract_Text_From_PDF(file="document.pdf")
+if __name__ == "__main__":
+    extract = Extract_Text_From_PDF(file_path="document.pdf")
     start_page = int(input("Enter Start Page : "))
     end_page = int(input("Enter End Page : "))
     extract.extract_text(start_page=start_page, end_page=end_page)
